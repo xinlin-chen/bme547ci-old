8a6f62acda0baaa20ee093024e115c759a392eac
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-06 13:09:58 -0500

    pep8 compliant version

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -1,8 +1,12 @@
 from tachycardia import is_tachycardic
 import pytest
 
+
 @pytest.mark.parametrize("input, exp",
-                         [('tachycardic', True), ('  tachycardic', True), ('  tachycardic', True), ('tachycardic.', True), ('tachycardi', False), ('TachyCardic', True), ('TACHYCARDIC', True), (6, False)])
+                         [('tachycardic', True), ('  tachycardic', True),
+                          ('tachycardic', True), ('tachycardic.', True),
+                          ('tachycardi', False), ('TachyCardic', True),
+                          ('TACHYCARDIC', True), (6, False)])
 def test_is_tachycardic(input, exp):
     ans = is_tachycardic(input)
     assert ans == exp
