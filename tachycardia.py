169b74deb481a2dcf2db7f3baaaa0079889360e6
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-06 12:33:00 -0500

    added code to reject non-string input

--- tachycardia.py
+++ tachycardia.py
@@ -2,7 +2,10 @@ import re
 
 
 def is_tachycardic(string_in):
-    string = string_in.lower()
+    if isinstance(string_in, str):
+        string = string_in.lower()
+    else:
+        return False
     if re.search('tachycardic', string):
         return True
     else:
