d5ced12bc7962c6dd14b35104be1ef33eac99de0
Janet Chen <janetchen@Janets-MacBook-Air.local>
2019-02-06 11:46:28 -0500

    pep8 compliant file

--- tachycardia.py
+++ tachycardia.py
@@ -1,10 +1,9 @@
 import re
 
+
 def is_tachycardic(string_in):
     string = string_in.lower()
     if re.search('tachycardic', string):
-        print('True')
         return True
     else:
-        print('False')
         return False
